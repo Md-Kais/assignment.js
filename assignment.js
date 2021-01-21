a27869ac32706de78b5b4b3a8ea0cc2cab9873d5
Md-Kais <mdkais3@gmail.com>
2021-01-22 04:12:29 +0600

    done all

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 // https://github.com/Md-Kais/assignment.js
 
 
-//kilometerToMeter
+//kilometerToMeter--------------------------------------------------->
 
 function kilometerToMeter(kilometer) {
     if (kilometer < 0) {
@@ -17,7 +17,7 @@ function kilometerToMeter(kilometer) {
 }
 // console.log(kilometerToMeter(13));//hoye gese
 
-//budgetCalculator
+//budgetCalculator--------------------------------------------------->
 
 //watch->$50,phone->$100,laptop->$500;
 //ouptut ->total $
@@ -43,6 +43,7 @@ function budgetCalculator(watch, phone, laptop) {
         }
         return invalid;
     }
+    //if things passed:
     else {
         let total = watch * 50 + phone * 100 + laptop * 500;
         return total;
@@ -51,6 +52,59 @@ function budgetCalculator(watch, phone, laptop) {
 }
 // console.log(budgetCalculator(1.0, 1.0, 19.000000000000000000));
 
+//hotelCost--------------------------------------------------->
 
+//1->10=>$100,11->20=>$80,>20=>$50;
 
+function hotelCost(day) {
+    var amount = 0;
+    if (day < 0) {
+        return "Day can't be negative, SIR!"
+    }
+    else if (day <= 10 && day >= 0) {
+        amount = Math.ceil(day) * 100;
+        return amount;
+    }
+    else if (day <= 20 && day > 10) {
+        amount = 10 * 100;///10
+        var reminder = Math.ceil(day - 10) * 80;///<20
+        amount += reminder;
+        return amount;
+    }
+    else if (day > 20) {
+        amount = 10 * 100;///10
+        amount += (10 * 80);///another 10 , <20
+        var reminder = Math.ceil(day - 20) * 50;//greater than 20;
+        amount += reminder;
+        return amount;
 
+    }
+}
+// console.log(hotelCost(21));
+
+//megaFriend--------------------------------------------------->
+
+//array
+//big friend(length);
+// only works for correct string array;
+
+function megaFriend(array) {
+    var len = 0;//length of element of an array
+    var ans = "";//return ans;
+    for (let i = 0; i < array.length; i++) {
+        var friend = array[i];
+        if (friend.length > len) {
+            len = friend.length;
+            ans = friend;
+        }
+    }
+    if (len === 0) {
+        return "There is no input or you have no friend or invalid input(numebers) or empty strings. Go and Find Some or give us correct input.";
+    }
+    else {
+        return ans;
+    }
+}
+// console.log(megaFriend(["","","",""]));
+
+// -------------------->END---------------------------->
